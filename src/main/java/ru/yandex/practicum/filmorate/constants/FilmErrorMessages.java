3c1b73e517ec60e6b4eb0fdd6398f79477d83d7b
Stas <bloodofabyss@yandex.ru>
2022-11-27 23:45:24 +0300

    chore: написал константы ошибок для юзеров и фильмов

---
+++ src/main/java/ru/yandex/practicum/filmorate/constants/FilmErrorMessages.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class FilmErrorMessages {
+    public static final String name = "Название не может быть пустым";
+    public static final String description = "Максимальная длина описания — 200 символов";
+    public static final String releaseDate = "Дата релиза — не раньше 28 декабря 1895 года";
+    public static final String duration = "Продолжительность фильма должна быть положительной";
+    public static final String notFound = "Фильм не найден";
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/constants/UserErrorMessages.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserErrorMessages {
+    public static final String email = "Электронная почта не может быть пустой и должна содержать символ @";
+    public static final String login = "Логин не может быть пустым и содержать пробелы";
+    public static final String birthday = "Дата рождения не может быть в будущем";
+    public static final String notFound = "Пользователь не найден";
+}
