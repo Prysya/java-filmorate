62220e24b66ce50155216faa9a25a5d9d8628823
Stas <bloodofabyss@yandex.ru>
2022-11-30 19:51:16 +0300

    chore: поменял текст ошибки в огшибках фильма

--- src/main/java/ru/yandex/practicum/filmorate/constants/FilmErrorMessages.java
+++ src/main/java/ru/yandex/practicum/filmorate/constants/FilmErrorMessages.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class FilmErrorMessages {
     public static final String name = "Название не может быть пустым";
-    public static final String description = "Максимальная длина описания — 200 символов";
+    public static final String description = "Превышена максимальная длина описания";
     public static final String releaseDate = "Дата релиза — не раньше 28 декабря 1895 года";
     public static final String duration = "Продолжительность фильма должна быть положительной";
     public static final String notFound = "Фильм не найден";
