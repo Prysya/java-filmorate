3aacacb2408577650adf237970cf50334713c2c2
Stas <bloodofabyss@yandex.ru>
2022-11-30 19:49:55 +0300

    chore: поменял энам месяца в валидации, а так же добавил валидацю по дате рождения

--- src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDateValidator.java
@@ -5,12 +5,12 @@ import ru.yandex.practicum.filmorate.annotation.validator.ReleaseDateValidation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
-import java.util.Calendar;
+import java.time.Month;
 
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDateValidation, LocalDate> {
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext cxt) {
-        LocalDate minDate = LocalDate.of(1895, Calendar.DECEMBER, 28);
-        return date.isAfter(minDate);
+        LocalDate minDate = LocalDate.of(1895, Month.DECEMBER, 28);
+        return date.isEqual(minDate) || date.isAfter(minDate);
     }
 }
