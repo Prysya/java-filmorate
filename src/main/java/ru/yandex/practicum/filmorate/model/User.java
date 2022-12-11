fec739db1b7ec6a794073ad8c0b5c73086bc3f16
Stas <bloodofabyss@yandex.ru>
2022-12-11 18:54:48 +0300

    chore: добавил лайки и друзей в модели юзеров и фильмов

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -9,17 +9,11 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class User {
-    /**
-     * Уникальный идентификатор
-     */
-    private Integer id = null;
-    /**
-     * Имя для отображения
-     */
-    private String name;
     /**
      * Электронная почта
      */
@@ -37,4 +31,16 @@ public class User {
      */
     @PastOrPresent(message = UserErrorMessages.birthday)
     private final LocalDate birthday;
+    /**
+     * Уникальный идентификатор
+     */
+    private Long id = null;
+    /**
+     * Имя для отображения
+     */
+    private String name;
+    /**
+     * Список уникальных идентификаторов друзей
+     */
+    private Set<Long> friends = new HashSet<>();
 }
