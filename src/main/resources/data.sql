844545b1b8f07206979f4bfc1661610df17a2914
Stas <bloodofabyss@yandex.ru>
2023-01-07 17:02:52 +0300

    feat: написал скрипты для заполнения таблицы, а затем рейтинга и жанров

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO PUBLIC.GENRE (GENRE_NAME)
+VALUES ('Комедия'),
+       ('Драма'),
+       ('Мультфильм'),
+       ('Триллер'),
+       ('Документальный'),
+       ('Боевик');
+
+INSERT INTO PUBLIC.MPA_RATING (MPA_RATING_NAME)
+VALUES ('G'),
+       ('PG'),
+       ('PG-13'),
+       ('R'),
+       ('NC-17');
