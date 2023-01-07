a3ece6f3b4c58fd13a5d38558061edd7b7268dcd
Stas <bloodofabyss@yandex.ru>
2023-01-07 17:12:38 +0300

    feat: добавил новые хендеры ошибок

--- src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
@@ -2,11 +2,14 @@ package ru.yandex.practicum.filmorate.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.yandex.practicum.filmorate.error.ErrorResponse;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.NotUniqueException;
 
 import java.util.Objects;
 
@@ -17,7 +20,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
         return new ErrorResponse(
-                e.getMessage()
+            e.getMessage()
         );
     }
 
@@ -25,15 +28,40 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(final MethodArgumentNotValidException e) {
         return new ErrorResponse(Objects.requireNonNull(e.getBindingResult().getFieldError()).getField() +
-                " " +
-                e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+            " " +
+            e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequest(final MethodArgumentTypeMismatchException e) {
+        return new ErrorResponse(
+            String.format("Параметр '%s' со значениeм '%s' не может быть приведен к типу '%s'", e.getName(),
+                e.getValue(), Objects.requireNonNull(
+                    e.getRequiredType()).getSimpleName()));
+    }
+
+    @ExceptionHandler(MissingPathVariableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequest(final MissingPathVariableException e) {
+        return new ErrorResponse(
+            String.format("Параметр: '%s' не передан", e.getVariableName())
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleNotUniqueException(final NotUniqueException e) {
+        return new ErrorResponse(
+            e.getMessage()
+        );
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
         return new ErrorResponse(
-                "Произошла непредвиденная ошибка."
+            "Произошла непредвиденная ошибка."
         );
     }
 }
