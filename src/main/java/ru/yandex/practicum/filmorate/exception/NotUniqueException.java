644a4aa38305857beda35621d28d70bcd5fd2aef
Stas <bloodofabyss@yandex.ru>
2023-01-07 17:03:49 +0300

    feat: написал ошибку уникальности значения

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotUniqueException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotUniqueException.java
@@ -3,6 +3,9 @@ package ru.yandex.practicum.filmorate.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.CONFLICT)
 public class NotUniqueException extends RuntimeException {
+    public NotUniqueException(String message) {
+        super(message);
+    }
 }
