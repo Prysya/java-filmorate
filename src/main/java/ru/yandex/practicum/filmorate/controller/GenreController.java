8ba02846ce74c6a7aef57c819cf60cf53c3af73d
Stas <bloodofabyss@yandex.ru>
2023-01-07 17:05:16 +0300

    feat: написал контроллеры жанров и рейтинга

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.service.GenreService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/genres")
+public class GenreController {
+    private final GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping
+    public List<Genre> getAll() {
+        return genreService.getAll();
+    }
+
+    @GetMapping("/{genreId}")
+    public Genre getById(@PathVariable Long genreId) throws NotFoundException {
+        return genreService.getById(genreId);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.MpaService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/mpa")
+public class MpaController {
+    private final MpaService mpaService;
+
+    @Autowired
+    public MpaController(MpaService mpaService) {
+        this.mpaService = mpaService;
+    }
+
+    @GetMapping
+    public List<Mpa> getAll() {
+        return mpaService.getAll();
+    }
+
+    @GetMapping("/{genreId}")
+    public Mpa getById(@PathVariable Long genreId) throws NotFoundException {
+        return mpaService.getById(genreId);
+    }
+}
