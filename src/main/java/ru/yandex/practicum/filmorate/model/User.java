821fda57e1680dd399adf3f6eb627253b8a30ec9
sstakhovskiy <sstakhovskiy@lanit.ru>
2023-01-12 11:24:56 +0300

    chore(User): добавил валидацию на null поля birthday

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import ru.yandex.practicum.filmorate.constant.UserErrorMessages;
 import ru.yandex.practicum.filmorate.utils.RegexPatterns;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -37,6 +34,7 @@ public class User {
     /**
      * Дата рождения
      */
+    @NotNull(message = UserErrorMessages.birthdayRequired)
     @PastOrPresent(message = UserErrorMessages.birthday)
     private final LocalDate birthday;
 }
